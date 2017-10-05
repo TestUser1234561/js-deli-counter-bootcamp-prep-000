f3767a81d25259f9ff2a884577c16552d3b11fd9
Morgan Fudge <mfudge92@gmail.com>
2017-10-05 04:51:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var katzDeli = [];
 
 function takeANumber(name) {
   if (typeof name == Array) {
-    
+    for(let people in name) {
+      
+    }
+  } else {
+    let number = katzDeli.push(name)
+    return `Welcome, ${name}. You are number ${number} in line.`
   }
 }
